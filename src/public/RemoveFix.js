777af94414e54c172ba70ab70b82369727a5d2ef
Blake Bradford <james.b.bradford@faa.gov>
2023-07-31 08:27:44 -0400

    Reset button styled and working

--- src/public/RemoveFix.js
+++ src/public/RemoveFix.js
@@ -6,10 +6,27 @@ function RemoveFix(fixId) {
   GatherData();
 }
 
-function ResetFixes() {
-  const fixElements = document.getElementsByClassName('card');
-  while (fixElements.length > 1) {
-    fixElements[1].remove();
-  }
-  GatherData();
+function ResetCards() {
+  swal(
+    'Resetting Fixes',
+    'Are you sure you want to reset all fixes?',
+    'warning',
+    {
+      buttons: {
+        cancel: 'No',
+        catch: {
+          text: 'Yes',
+          value: 'Yes',
+        },
+      },
+    }
+  ).then((value) => {
+    if (value === 'Yes') {
+      const fixElements = document.getElementsByClassName('card');
+      while (fixElements.length > 1) {
+        fixElements[1].remove();
+      }
+      GatherData();
+    }
+  });
 }
