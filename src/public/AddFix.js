be8941fbe5a4d5919316fb304a3fef1496929aa8
Blake Bradford <james.b.bradford@faa.gov>
2023-07-31 12:17:48 -0400

    Speed labels now toggle correctly

--- src/public/AddFix.js
+++ src/public/AddFix.js
@@ -15,9 +15,15 @@ add_fix.addEventListener('click', () => {
   let speedChecks = new_fix.getElementsByClassName('SpeedChecks');
   for (let i = 0; i < speedChecks.length; i++) {
     speedChecks[i].checked = false;
+    speedChecks[i].id = `${speedChecks[i].id}-${new_fix.id}`;
+  }
+  let speedLabels = new_fix.getElementsByClassName('SpeedLabel');
+  for (let i = 0; i < speedLabels.length; i++) {
+    speedLabels[i].htmlFor = `${speedChecks[i].id}`;
   }
   let fixInput = new_fix.getElementsByClassName('FixInput')[0];
   fixInput.value = '';
+
   container.appendChild(new_fix);
 
   reset_cards.style.display = 'block';
